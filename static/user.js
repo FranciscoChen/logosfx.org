abeabce4516782e2528a8cb66fcd1cd175144de6
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2024-08-19 17:25:13 +0000

    Kico: check if logged in and home css fix

--- static/user.js
+++ static/user.js
@@ -1,1 +1,17 @@
 // xhr to see if user is logged in, modify login button accordingly
+function user() {
+  var xhr = new XMLHttpRequest;
+  xhr.open("POST", "/loggedin", true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (this.readyState != 4)
+      return;
+    if (this.status == 200) {
+      if (this.responseText === '1') {
+        const btn = document.getElementsByClassName("site-buttons")[0].firstChild;
+        btn.innerText = '主页'
+        btn.href = '/home'
+      }
+    }
+  }
+}
