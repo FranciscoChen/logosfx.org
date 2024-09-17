b431daf846161d47780886928458837855ccf029
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2024-09-17 16:47:53 +0000

    Kico: education form test

---
+++ static/education.js
@@ -0,0 +1,17 @@
+function education(){
+  const s2 = document.getElementById('toggle-section2')
+  const s3 = document.getElementById('toggle-section3')
+  const s4 = document.getElementById('toggle-section4')
+  s2.addEventListener("click", function() {
+    this.classList.toggle('hidden')
+    document.getElementbyId('section2').classList.toggle('hidden')
+  }
+  s3.addEventListener("click", function() {
+    this.classList.toggle('hidden')
+    document.getElementbyId('section3').classList.toggle('hidden')
+  }
+  s4.addEventListener("click", function() {
+    this.classList.toggle('hidden')
+    document.getElementbyId('section4').classList.toggle('hidden')
+  }
+}
