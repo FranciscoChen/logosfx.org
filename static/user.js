516fa380c2dc37be8924197062304eaf3168aa0e
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2024-12-10 22:25:10 +0000

    Kico: 404 and 401 pages and fixes

--- static/user.js
+++ static/user.js
@@ -8,7 +8,6 @@ function user() {
       return;
     if (this.status == 200) {
         const btn = document.getElementsByClassName("site-buttons")[0].firstChild;
-        const auth = document.getElementById("authenticated")
       if (this.responseText === '1') {
         btn.innerText = '主页'
         btn.href = '/home'
@@ -18,10 +17,6 @@ function user() {
         btn.innerText = '登录'
         btn.href = '/login'
         btn.classList.add('opaque')
-        if (auth !== null) {
-	  auth.innerHTML = '<h1>请先登录</h1>';
-	  auth.classList.remove('not-visible');
-	}
       }
     }
   }
