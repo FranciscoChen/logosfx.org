1b2180aa7157d207b3c8b025e28e375fadf5aad8
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-01-15 17:40:38 +0000

    Kico: fix js for the educational background application info

--- static/education.js
+++ static/education.js
@@ -15,6 +15,23 @@ function addsection(){
     document.getElementById('section4').classList.toggle('hidden')
   })
 }
+function expandsection(){
+  const s2 = document.getElementById('toggle-section2')
+  const s3 = document.getElementById('toggle-section3')
+  const s4 = document.getElementById('toggle-section4')
+  if (document.getElementsByName('institution2')[0].value.length > 1) {
+    s2.classList.toggle('hidden')
+    document.getElementById('section2').classList.toggle('hidden')
+  } 
+  if (document.getElementsByName('institution3')[0].value.length > 1) {
+    s3.classList.toggle('hidden')
+    document.getElementById('section3').classList.toggle('hidden')
+  } 
+  if (document.getElementsByName('institution4')[0].value.length > 1) {
+    s4.classList.toggle('hidden')
+    document.getElementById('section4').classList.toggle('hidden')
+  } 
+}
 function getform(){
   var xhr = new XMLHttpRequest;
   xhr.open("POST", "/geteducationinfo", true);
@@ -45,6 +62,7 @@ function getform(){
           }
         }
       }
+      if (typeof expandsection === 'function') {expandsection()}
     }
   }
 }
