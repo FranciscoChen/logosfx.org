5a7e51f58e57c698da40171272750d6b1741061f
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-01-15 13:54:24 +0000

    Kico: prefill form js

--- static/studentinfo.js
+++ static/studentinfo.js
@@ -6,7 +6,26 @@ function getform(){
     if (this.readyState != 4)
       return;
     if (this.status == 200) {
-      console.log(this.responseText)
+      const data = this.responseText[0]
+      for (name in data) {
+        const ele = document.getElementsByName(name)[0]
+        if (typeof ele !== 'undefined') {
+          if (ele.type === 'checkbox') {
+            ele.checked = {'true':1,'false':0}[data[name]];
+          }
+          if (ele.type === 'text') {
+            ele.value = data[name]
+          }
+          if (ele.type === 'radio') {
+            const elearr = document.getElementsByName(name)
+            for (var i = elearr.length; i--){
+              if (elearr[i].name === data[name]){
+                elearr[i].checked = 1
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
