c0d275510c4ca96c3230667ca4ece43422fd8bc0
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-01-15 14:37:03 +0000

    Kico: increase font size

--- static/education.js
+++ static/education.js
@@ -15,3 +15,36 @@ function addsection(){
     document.getElementById('section4').classList.toggle('hidden')
   })
 }
+function getform(){
+  var xhr = new XMLHttpRequest;
+  xhr.open("POST", "/geteducationinfo", true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (this.readyState != 4)
+      return;
+    if (this.status == 200) {
+      const data = JSON.parse(this.responseText)[0]
+      for (name in data) {
+        const ele = document.getElementsByName(name)[0]
+        if (typeof ele !== 'undefined') {
+          if (ele.type === 'checkbox') {
+            ele.checked = {'true':1,'false':0}[data[name]];
+          }
+          if (ele.type === 'text') {
+            ele.value = data[name]
+          }
+          if (ele.type === 'radio') {
+            const elearr = document.getElementsByName(name)
+            const len = elearr.len
+            for (var i = 0; i < len; i++){
+              if (elearr[i].value === data[name]){
+                elearr[i].checked = 1;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
