a2e49ab493afe5d7e1d74bc2879aca559e2a4a72
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-01-16 21:30:09 +0000

    Kico: fix several js bugs

---
+++ static/applystudent.js
@@ -0,0 +1,36 @@
+function getform(){
+  var xhr = new XMLHttpRequest;
+  xhr.open("POST", "/getapplyinfo", true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (this.readyState != 4)
+      return;
+    if (this.status == 200) {
+      const data = JSON.parse(this.responseText)[0]
+      for (name in data) {
+        const ele = document.getElementsByName(name)[0]
+        if (typeof ele !== 'undefined') {
+          if (ele.type === 'checkbox') {
+            ele.checked = {'true':1,'false':0}[data[name]];
+          }
+          if (ele.type === 'text') {
+            ele.value = data[name]
+          }
+          if (ele.type === 'date') {
+            ele.value = data[name].slice(0,4)+'-'+data[name].slice(5,7)+'-'+data[name].slice(8,10)
+          }
+          if (ele.type === 'radio') {
+            const elearr = document.getElementsByName(name)
+            const len = elearr.length
+            for (var i = 0; i < len; i++){
+              if (elearr[i].value === data[name]){
+                elearr[i].checked = 1;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
