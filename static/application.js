e0f24a0c655274b93dae28f5c820f2cd7fcb07e0
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-01-20 17:45:58 +0000

    Kico: test application confirmation page info

--- static/application.js
+++ static/application.js
@@ -17,28 +17,22 @@ function fillapply(){
       const data = JSON.parse(this.responseText)[0]
       console.log(data);
       for (name in data) {
-        const dataref = data[name]
+        var dataref = data[name]
         var ele
         ele = document.getElementById(name)
-        if (name === 'maritalstatus') {
-          ele = document.getElementById(dataref)
+        if (name === 'gender') {
+          dataref = {man:'男',woman:'女'}[data[name]]
+        }
+        if (name === 'birthday') {
+          dataref = data[name].slice(0,4)+'年 '+data[name].slice(5,7)+'月 '+data[name].slice(8,10)+'日'
+        }
+        if (name === 'course') {
+          dataref = {0:'「基督教神学学士班」（B.Th.）',1:'「基督教教牧学士班」（B.Min.）',2:'「教会领袖证书班」（Certificate）',3:'特別学生'}[data[name]]
         }
         if (ele !== null){
-          if (ele.classList.contains('circle-around-anchor')){
-            if (dataref === true || name === 'maritalstatus') {
-              ele.classList.remove('hidden')
-            }
-          } else {
             const inforow  = document.createElement("p");
             inforow.innerHTML = dataref
             ele.append(inforow)
-          }
-          if (name === 'children' && dataref !== null && dataref.length > 0){
-            document.getElementById('ifchildren').classList.remove('hidden')
-          }
-          if (name === 'partnername' && dataref !== null && dataref.length > 0){
-            document.getElementById('ifmarried').classList.remove('hidden')
-          }
         }
       }
     }
