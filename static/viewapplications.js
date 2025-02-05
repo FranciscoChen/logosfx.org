3b680392a286d267f4c4a703549519d80991f3c7
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-02-05 22:37:54 +0000

    Kico: change view of managing submitted applications

--- static/viewapplications.js
+++ static/viewapplications.js
@@ -11,20 +11,20 @@ function  getsubmittedapplications(){
       return;
     if (this.status == 200) {
       const data = JSON.parse(this.responseText)
-      var totalrows = 0;
+      const table = document.getElementsByClassName("table")[0]
       for (rownumber in data){
         const row = data[rownumber]
-        const sethomecard  = document.createElement("div");
-        sethomecard.classList.add('home-card-wrap')
-        sethomecard.innerHTML = '<div class="home-card"><h1>入学申请表</h1><h3>姓：</h3><div><p>'+row.surname+'</p></div><h3>名字：</h3><div><p>'+row.name+'</p></div><h3>性别：</h3><div><p>'+{man:"男",woman:"女"}[row.gender]+'</p></div><h3>生日：</h3><div><p>'+row.birthdate.slice(0,4)+'年 '+row.birthdate.slice(5,7)+'月 '+row.birthdate.slice(8,10)+'日</p></div><h3>提交日期：</h3><div><p>'+row.submittedtimestamp.slice(0,4)+'年 '+row.submittedtimestamp.slice(5,7)+'月 '+row.submittedtimestamp.slice(8,10)+'日</p></div><a class="button button-area colorbutton" href="/home/menu/applications/view?id='+row.id+'">检查</a></div>'
-        document.getElementsByClassName("cards")[0].appendChild(sethomecard);
-        totalrows++
-      }
-      if (totalrows === 0) {
-        const sethomecard  = document.createElement("div");
-        sethomecard.classList.add('home-card-wrap')
-        sethomecard.innerHTML = '<div class="home-card"><h3>目前没有提交可查看的申请表</h3></div>'
-        document.getElementsByClassName("cards")[0].appendChild(sethomecard);
+        const tablerow  = document.createElement("tr");
+        sethomecard.innerHTML = '<td>'+row.surname+' '+row.name'</td>'+
+        '<td><a class="button button-area '+row.verified ? 'green' : 'red' +'button" href="/home/menu/applications/view?id='+row.id+'">检查</a></div></td>'
+        '<td><a class="button button-area '+row.photo ? 'green' : 'red' +'button" href="/home/menu/photos/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.educationcertificate ? 'green' : 'red' +'button" href="/home/menu/educationcertificate/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.faithsignature ? 'green' : 'red' +'button" href="/home/menu/faithsignature/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.testimony ? 'green' : 'red' +'button" href="/home/menu/testimony/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.churchrecommendation ? 'green' : 'red' +'button" href="/home/menu/churchrecommendation/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.elderrecommendation1 ? 'green' : 'red' +'button" href="/home/menu/elderrecommendation1/view?id='+row.id+'">查看</a></div></td>'
+        '<td><a class="button button-area '+row.elderrecommendation2 ? 'green' : 'red' +'button" href="/home/menu/elderrecommendation2/view?id='+row.id+'">查看</a></div></td>'
+        table.appendChild(tablerow);
       }
     }
   }
