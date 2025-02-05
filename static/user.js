b388675a313b32bcab9023749d4c45eacaca3445
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-02-05 15:14:45 +0000

    Kico: add logout button

--- static/user.js
+++ static/user.js
@@ -9,19 +9,19 @@ function user() {
     if (this.status == 200) {
         const btn = document.getElementsByClassName("site-buttons")[0].firstChild;
       if (this.responseText === '1') {
-        btn.innerText = '主页'
-        btn.href = '/home'
-        btn.classList.add('opaque')
-      } else {
         if (new URL(window.location.href).pathname === '/home') {
           btn.innerText = '注销'
           btn.href = '/logout'
           btn.classList.add('opaque')
 	} else {
-          btn.innerText = '登录'
-          btn.href = '/login'
+          btn.innerText = '主页'
+          btn.href = '/home'
           btn.classList.add('opaque')
 	}
+      } else {
+        btn.innerText = '登录'
+        btn.href = '/login'
+        btn.classList.add('opaque')
       }
     }
   }
