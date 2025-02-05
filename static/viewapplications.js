cd1479deea4c3691560478b6d5d1ae7ecc067875
Ubuntu <ubuntu@vps-75c3a192.vps.ovh.net>
2025-02-05 23:02:54 +0000

    Kico: fix table js

--- static/viewapplications.js
+++ static/viewapplications.js
@@ -15,7 +15,7 @@ function  getsubmittedapplications(){
       for (rownumber in data){
         const row = data[rownumber]
         const tablerow  = document.createElement("tr");
-        sethomecard.innerHTML = '<td>'+row.surname+' '+row.name+'</td>'+
+        tablerow.innerHTML = '<td>'+row.surname+' '+row.name+'</td>'+
         '<td><a class="button button-area '+row.verified ? '' : 'red' +' colorbutton" href="/home/menu/applications/view?id='+row.id+'">检查</a></div></td>'
         '<td><a class="button button-area '+row.photo ? '' : 'red' +' colorbutton" href="/home/menu/photos/view?id='+row.id+'">查看</a></div></td>'
         '<td><a class="button button-area '+row.educationcertificate ? '' : 'red' +' colorbutton" href="/home/menu/educationcertificate/view?id='+row.id+'">查看</a></div></td>'
