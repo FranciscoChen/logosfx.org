3cc6c6697bc6411196b3aa2d0d7bf3621eae223a
Ubuntu <ubuntu@chessil.com>
2025-06-01 19:12:36 +0000

    Kico: fix unblur js

--- dynamic/load.js
+++ dynamic/load.js
@@ -29,12 +29,21 @@ window.onload = () => {
   }
 }
 
+function unblurElements(elementArray){
+  const length = elementArray.length
+  if (length > 0){
+    for (var i = 0; i < length; i++){
+      elementArray[i].classList.add('unblur');
+    }
+  }
+}
 function unblur(){
   if (document.fonts.check("12px FZKai") === true) {
-    document.getElementsByTagName('h1').forEach((element) => element.classList.add('unblur'));
-    document.getElementsByTagName('h2').forEach((element) => element.classList.add('unblur'));
-    document.getElementsByTagName('h3').forEach((element) => element.classList.add('unblur'));
-    document.getElementsByClassName('headings-font').forEach((element) => element.classList.add('unblur'));
+    unblurElements(document.getElementsByTagName('h1'))
+    unblurElements(document.getElementsByTagName('h2'))
+    unblurElements(document.getElementsByTagName('h3'))
+    unblurElements(document.getElementsByTagName('h1'))
+    unblurElements(document.getElementsByClassName('headigns-font'))
     clearInterval(unblurInterval)
   }
 }
