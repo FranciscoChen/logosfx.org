85c22ccd0b793a91a56768a6cb4c6c4b37d727bf
Ubuntu <ubuntu@chessil.com>
2025-07-02 18:24:33 +0000

    test and fix photos modal box

--- dynamic/photos.js
+++ dynamic/photos.js
@@ -2,6 +2,9 @@ function photoexpand(){
   var modal = document.getElementById("myModal");
   const elements = document.getElementsByClassName('photo-wrap')
   var elelen = elements.length
+  modal.onclick = function() {
+    modal.classList.toggle('modal-display')
+  }
   for (var i = 0; i < elelen; i++){
     elements[i].onclick = function(evt) {
       document.getElementById('modal-photo').src = evt.target.src
