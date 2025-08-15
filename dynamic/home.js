d035b179f4b2bb67ed68162dc3bf6b249ebc6c93
Ubuntu <ubuntu@chessil.com>
2025-08-15 09:26:44 +0000

    let user modify signup data after reviewing

--- dynamic/home.js
+++ dynamic/home.js
@@ -204,7 +204,12 @@ function ready(){
     if (this.readyState != 4)
       return;
     if (this.status == 200) {
-      if (this.responseText === '0') {
+      if (this.responseText === '1') {
+        const sethomecard  = document.createElement("div");
+        sethomecard.classList.add('home-card-wrap')
+        sethomecard.innerHTML = '<div class="home-card"><h2>提交申请表</h2><p>提交前请仔细检查您的所有信息是否准确（一旦发送，之后将无法编辑）</p><div class="application-buttons"><div id="studentinfo" class="button-wrap"><a class="button button-area colorbutton" href="/home/application">检查信息</a></div></div></div>'
+        document.getElementsByClassName("cards")[0].appendChild(sethomecard);
+      }
         const sethomecard  = document.createElement("div");
         sethomecard.classList.add('home-card-wrap')
         sethomecard.innerHTML = '<div class="home-card"><h2>注册流程</h2><p>您尚未填写注册表，请继续注册流程</p><div class="application-buttons"><div id="studentinfo" class="button-wrap"><div class="exclamation-bubble-anchor hidden"><div class="exclamation-bubble">!</div></div><a class="button button-area colorbutton" href="/home/studentinfo">个人信息</a></div><div id="education" class="button-wrap"><div class="exclamation-bubble-anchor hidden"><div class="exclamation-bubble">!</div></div><a class="button button-area colorbutton" href="/home/education">教育资料</a></div><div id="churchlife" class="button-wrap"><div class="exclamation-bubble-anchor hidden"><div class="exclamation-bubble">!</div></div><a class="button button-area colorbutton" href="/home/churchlife">教会资料</a></div><div id="consultants" class="button-wrap"><div class="exclamation-bubble-anchor hidden"><div class="exclamation-bubble">!</div></div><a class="button button-area colorbutton" href="/home/consultants">咨询信息</a></div></div></div>'
@@ -213,13 +218,6 @@ function ready(){
         education();
         churchlife();
         consultants();
-      }
-      if (this.responseText === '1') {
-        const sethomecard  = document.createElement("div");
-        sethomecard.classList.add('home-card-wrap')
-        sethomecard.innerHTML = '<div class="home-card"><h2>提交申请表</h2><p>提交前请仔细检查您的所有信息是否准确（一旦发送，之后将无法编辑）</p><div class="application-buttons"><div id="studentinfo" class="button-wrap"><a class="button button-area colorbutton" href="/home/application">检查信息</a></div></div></div>'
-        document.getElementsByClassName("cards")[0].appendChild(sethomecard);
-      }
     }
   }
 }
